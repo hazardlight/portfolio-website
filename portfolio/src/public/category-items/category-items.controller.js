2dd0eabb8d55ed4f078c10fbbc2df5a921827c31
InfiniteZero <toph4tbandit@gmail.com>
2018-02-21 15:27:31 -0500

    adding contact form stuff

--- portfolio/src/public/category-items/category-items.controller.js
+++ portfolio/src/public/category-items/category-items.controller.js
@@ -1,3 +1,22 @@
+(function () {
+"use strict";
+
+angular.module('public')
+.controller('CategoryItemsController', CategoryItemsController);
+
+CategoryItemsController.$inject = ['categoryItems', 'PortfolioService'];
+function CategoryItemsController(categoryItems, PortfolioService) {
+  var $ctrl = this;
+  $ctrl.categoryItems = categoryItems;
+
+  // console.log($ctrl.categoryItems[1].images[1]);
+
+// this controller serves category-items.html and helpbot.html with categoryItems
+}
+
+})();
+
+
 // (function () {
 // "use strict";
 //
@@ -21,22 +40,3 @@
 // }
 //
 // })();
-
-
-(function () {
-"use strict";
-
-angular.module('public')
-.controller('CategoryItemsController', CategoryItemsController);
-
-CategoryItemsController.$inject = ['categoryItems', 'PortfolioService'];
-function CategoryItemsController(categoryItems, PortfolioService) {
-  var $ctrl = this;
-  $ctrl.categoryItems = categoryItems;
-
-  // console.log($ctrl.categoryItems[1].images[1]);
-
-// this controller serves category-items.html and helpbot.html with categoryItems
-}
-
-})();
---
+++ portfolio/src/public/contact/contact.controller.js
@@ -0,0 +1,12 @@
+(function () {
+"use strict";
+
+angular.module('public')
+.controller('ContactController', ContactController);
+
+ContactController.$inject = [];
+function ContactController() {
+  var $ctrl = this;
+
+}
+})();
