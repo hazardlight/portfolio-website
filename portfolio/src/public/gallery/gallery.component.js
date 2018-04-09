d5849450815b221964b9f230c9c3520b6e93f166
unknown <ctabis@gmail.com>
2018-04-09 06:34:53 -0400

    added active image indicator to thumbnails on gallery pages

--- portfolio/src/public/gallery/gallery.component.js
+++ portfolio/src/public/gallery/gallery.component.js
@@ -14,14 +14,35 @@ GalleryItemController.$inject = [];
 function GalleryItemController() {
   var $ctrl = this;
 
-  this.active = "";
+  // this.active = "";
+
+  this.thumbnailActive = 0;
+
+  this.count = 0;
 
   $ctrl.slide = function (dir) {
     $('#galleryCarousel').carousel(dir);
+    if(dir === 'next')
+      if(this.thumbnailActive === this.count){
+        this.thumbnailActive = 0;
+      }
+      else{
+        this.thumbnailActive++;
+      }
+    if(dir === 'prev')
+        if(this.thumbnailActive === 0){
+          this.thumbnailActive = this.count;
+        }
+        else{
+          this.thumbnailActive--;
+        }
+
   }
 
   $ctrl.slideTo = function (int) {
     $('#galleryCarousel').carousel(int);
+    this.thumbnailActive = int;
+    console.log(this.count);
   }
   $ctrl.active = function (int) {
     var activeFlag = "";
